3fbb6d14a904c2dad46921b69ff8503e34188b3f
jessicasproul18-dot <jessicasproul18@gmail.com>
2026-04-18 20:49:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -165,5 +165,7 @@ function updateAndDrawConfetti() { confetti.forEach(c => { c.y += c.speed; c.x +
 function drawBackground() { bgDecorations.forEach(bg => { bg.x -= bg.speed; if (bg.x < -50) bg.x = canvas.width + 50; ctx.fillStyle = bg.color; ctx.beginPath(); ctx.ellipse(bg.x, bg.y, bg.size * 0.8, bg.size, 0, 0, Math.PI * 2); ctx.fill(); ctx.strokeStyle = "rgba(0,0,0,0.2)"; ctx.beginPath(); ctx.moveTo(bg.x, bg.y + bg.size); ctx.lineTo(bg.x, bg.y + bg.size + 20); ctx.stroke(); }); }
 
 typeMessage(); introLoop();
+introCatImg.style.left = "400px";
+introCatImg.style.top = "50px";
 
 
